ad549fd34ef3124dba42ab6aad96204ba7b66574
harisharumugam94 <priyaharish055@gmail.com>
2026-07-11 12:03:27 +0530

    adding sommefile in the java and change the file in the database folder

---
+++ java/basics/variable.java
@@ -0,0 +1,10 @@
+public class variable {
+    public static void main(String[] args)
+{
+    //syntax for variable: datatype variablename = value;
+    int v=24;
+    int p=90;
+    System.out.print(v);
+    System.out.println(p);
+}    
+}
